df3c2d3ab1338017d8f10191eae5073b67058f8d
Ildar Timerbaev <itimerbaev@icloud.com>
2025-03-19 22:36:32 +0300

    Attempt to change TON derivation path

--- src/mutations/use-wallet-create.ts
+++ src/mutations/use-wallet-create.ts
@@ -9,7 +9,7 @@ const PATHS = {
   [EcencyWalletCurrency.BTC]: "m/44'/0'/0'/0/0", // Bitcoin (BIP44)
   [EcencyWalletCurrency.ETH]: "m/44'/60'/0'/0/0", // Ethereum (BIP44)
   [EcencyWalletCurrency.SOL]: "m/44'/501'/0'/0'", // Solana (BIP44)
-  [EcencyWalletCurrency.TON]: "m/44'/396'/0'/0/0", // TON (BIP44)
+  [EcencyWalletCurrency.TON]: "m/44'/607'/0'", // TON (BIP44)
   [EcencyWalletCurrency.TRON]: "m/44'/195'/0'/0/0", // Tron (BIP44)
   [EcencyWalletCurrency.APT]: "m/44'/637'/0'/0'/0'", // Aptos (BIP44)
   [EcencyWalletCurrency.ATOM]: "m/44'/118'/0'/0/0", // Cosmos (BIP44)
