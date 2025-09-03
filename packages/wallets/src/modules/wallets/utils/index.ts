abc477777b5987bb589260db98a6a7429913d885
feruz <fmiboy@gmail.com>
2025-09-03 10:09:13 +0300

    fix double export

--- packages/wallets/src/modules/wallets/utils/index.ts
+++ packages/wallets/src/modules/wallets/utils/index.ts
@@ -4,4 +4,3 @@ export * from "./mnemonic-to-seed-bip-39";
 export * from "./derive-hive-bip44-keys";
 export * from "./derive-hive-master-password-keys";
 export * from "./detect-hive-key-derivation";
-export * from "./derive-hive-bip44-keys";
